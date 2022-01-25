bda88a94562696527254c9645d714871014a8357
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-25 20:28:12 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,2 +1,5 @@
 import mysql.connector as mysql
 
+def get_user_data():
+  user_data=[['TK', 'akstr!admin2'],['PB','akstr!admin2'],['NV',null],['DB',null],['FLG','baddragon'],['SHK',null],['TB',null],['TT',null],['RS',null]]
+return user_data
