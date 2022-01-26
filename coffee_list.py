745224ce24537726293301c7392f31ab0d32dd0d
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-26 16:17:30 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -268,6 +268,7 @@ if logged_in == True or logged_in == False:
 
         fig8 = px.bar(df, x='percentage', y=names, title="Total percentages of breaks", labels={"y":"", "count":"Percentage", "variable":"drinkers"}, text='percentage', text_auto=True, orientation='h')
         fig8.update_layout(title_font_size=24, showlegend=False)
+        fig8.update_traces(hovertemplate='%{x} <br>%{x}<br>Percentage: %{y}')
         col6.plotly_chart(fig8, use_container_width=True)
 
     #-------------------------------------------------------------------------------------------------------------- cumulated coffees monthly (line chart)
