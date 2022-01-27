8db9ea5100ccdc058ede7f66d7293ad8ef7c720d
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 21:43:08 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -67,11 +67,11 @@ if login:
         else:
             st.sidebar.write("Member status: User") 
     else:
-        st.title("Welcome to our coffee list")
+        st.title("Welcome to the future of coffee drinking")
         st.write("In order to get access to the visualised data you need to be logged in with your username and password.")
         st.warning("Incorrect username/password")
 else:
-    st.title("Welcome to our coffee list")
+    st.title("Welcome to the future of coffee drinking")
     st.write("In order to get access to the visualised data you need to be logged in with your username and password.")
     
 
