885a4679f698f7fe00e41b927e15443302eb9cd2
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 16:20:54 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -96,7 +96,8 @@ if logged_in == True:
         st.markdown("You can change your password here.")
         col1,col2,col3 = st.columns([0.5,1,0.7])
         curr_user = col2.text_input("Current password", type="password", placeholder = "Old password")
-        col2.write("-" * 34)
+        st.write("-" * 34)
+        col1,col2,col3 = st.columns([0.5,1,0.7])
         user_pw = col2.text_input("Choose a new password", type="password", placeholder = "New password")
         new_user = col2.text_input("Repeat the new password", type="password", placeholder = "Repeat password")
         
