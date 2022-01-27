0bc190eba1bcd5bd8a77f273ad37bd53d03e73cf
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-27 21:50:02 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -74,9 +74,10 @@ else:
     st.write("In order to get access to the visualised data you need to be logged in with your username and password.")
     
 
-if logged_in == True and admin_status != 1:
-    profile_nav = st.sidebar.selectbox("Profile Options", ("Show diagrams","Enter holidays","Change username","Change password"), 0)
-elif logged_in == True and admin_status == 1:
+if logged_in == True
+    if admin_status != 1:
+        profile_nav = st.sidebar.selectbox("Profile Options", ("Show diagrams","Enter holidays","Change username","Change password"), 0)
+    elif logged_in == True and admin_status == 1:
     profile_nav = st.sidebar.selectbox("Profile Options", ("Show diagrams","Submit coffee break","Delete coffee break","Enter holidays","Change username","Change password"), 0)
     
     if profile_nav == "Enter holidays":                                             # Enter holidays page
