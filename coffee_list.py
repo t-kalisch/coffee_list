ec1b6073c985a0d0cd877054c8cd9d908d42971e
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-29 14:55:46 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -441,7 +441,7 @@ if logged_in == True and profile_nav == "Show diagrams":
         
         fig10 = px.line(df, title="Number of coffees per month per person", labels={"variable":"", "index":"", "value":"Number of coffees"})
         fig10.update_traces(hovertemplate='%{x}: %{y} %')
-        fig10.update_layout(hovermode="x"legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+        fig10.update_layout(hovermode="x", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
         st.plotly_chart(fig10, use_container_width=True)        
       
     
