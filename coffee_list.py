8665831bdc9d00805785b8226e0cf16d53a37323
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-01-29 14:56:36 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -435,12 +435,17 @@ if logged_in == True and profile_nav == "Show diagrams":
         st.plotly_chart(fig10, use_container_width=True)
         
         fig10 = px.line(df, title="Number of coffees per month per person", labels={"variable":"", "index":"", "value":"Number of coffees"})
-        fig10.update_traces(hovertemplate='%{x}: %{y} %')
+        fig10.update_traces(hovertemplate='%{y} coffees')
         fig10.update_layout(hovermode="x unified", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
         st.plotly_chart(fig10, use_container_width=True)
-        
+ 
         fig10 = px.line(df, title="Number of coffees per month per person", labels={"variable":"", "index":"", "value":"Number of coffees"})
-        fig10.update_traces(hovertemplate='%{x}: %{y} %')
+        fig10.update_traces(hovertemplate='%{y}')
+        fig10.update_layout(hovermode="x unified", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
+        st.plotly_chart(fig10, use_container_width=True)
+
+        fig10 = px.line(df, title="Number of coffees per month per person", labels={"variable":"", "index":"", "value":"Number of coffees"})
+        fig10.update_traces(hovertemplate='%{y}')
         fig10.update_layout(hovermode="x", legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
         st.plotly_chart(fig10, use_container_width=True)        
       
