c44e9cd2fe532609159062380720540e7abe82af
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-02 14:19:10 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -369,30 +369,12 @@ if logged_in == True and profile_nav == "Show diagrams":
 
         df = pd.DataFrame(temp2_abs, columns=columns_corr)
         fig5 = px.scatter(df, x='x-values', y='y-values', size='size', labels={"x-values":"", "y-values":""}, title="Absolute correlation", color='size')
-
-        #df = pd.DataFrame(temp2, columns=columns_corr)
-
-        #fig5 = px.scatter(df, x='x-values', y='y-values', size='size', labels={"x-values":"", "y-values":""}, title="Absolute correlation", color='size')#, text='size')
         fig5.update_layout(title_font_size=24, showlegend=False, xaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names), yaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names))
         #fig5.update_traces(hovertemplate='<br>%{x} with %{y}<br>' + '%{size}')
         fig5.update_xaxes(side="top")
         col3.plotly_chart(fig5, use_container_width=True)#              absolute correlation
         #                                                  --------------------------------------------------
-        temp=[]#                                                        relative correlation
-        temp1=[]
-        temp2=[]
-        tickval_num=[]
-        for i in range(len(names)):
-           tickval_num.append(i+1)
-           for j in range(len(names)):
-               temp=[]
-               temp.append(i+1)
-               temp.append(j+1)
-               temp.append(round(100*corr_abs[i][j]/total_coffees[i],1))         #!!!!  Calculates relative correlation; uses total_coffees  !!!!
-               temp2.append(temp)
-
-        df = pd.DataFrame(temp2, columns=columns_corr)
-
+        df = pd.DataFrame(temp2_rel, columns=columns_corr)
         fig6 = px.scatter(df, x='x-values', y='y-values', size='size', labels={"x-values":"", "y-values":""}, title="Relative correlation", color='size')#, text='size')
         fig6.update_layout(title_font_size=24, showlegend=False, xaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names), yaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names))
         fig6.update_traces(hovertemplate='<br>%{x} with %{y}<br>Percentage: %{size}')
