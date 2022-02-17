9796634ab3cdab354724eb79388d15edb8511b69
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-17 18:17:01 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -480,7 +480,7 @@ if cookie_manager:
         cookie = st.text_input("Cookie", key="1")
         val = st.text_input("Value")
         if st.button("Add"):
-            cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+            cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=18))
     with c3:
         st.subheader("Delete Cookie:")
         cookie = st.text_input("Cookie", key="2")
