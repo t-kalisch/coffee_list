0731b402166e06461136acdfd13c81539ae377b7
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-17 19:32:20 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -72,17 +72,15 @@ if login:
             logged_in = True
             admin_status=user_data[i][2]
     if logged_in == True:
-        cookie_manager.set("logged_in", True, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_true")
+        cookie_manager.set("logged_in", "false", expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_true")
     else:
-        cookie_manager.set("logged_in", False, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_false")
-        logged_in = False
+        cookie_manager.set("logged_in", "false", expires_at=datetime.datetime(year=2030, month=1, day=1), key="logged_in_false")
 
 if logout:
-    cookie_manager.set("logged_in", False, expires_at=datetime.datetime(year=2030, month=1, day=1), key="logout")
-    logged_in = False
+    cookie_manager.set("logged_in", "false", expires_at=datetime.datetime(year=2030, month=1, day=1), key="logout")
             
 
-if cookie_manager.get(cookie="logged_in") == True:
+if logged_in == "true":
     st.title("Logged in as {}".format(user))
     if admin_status == 1:
         col2.write("  Status: Administrator")
