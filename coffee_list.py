11d104a2cff35b336658afa9785cb56943b01188
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-17 18:14:15 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -451,6 +451,42 @@ if logged_in == True and profile_nav == "Show diagrams":
       
 
     
+    
+if cookie_manager:
+    st.write("# Cookie Manager")
+
+    @st.cache(allow_output_mutation=True)
+    def get_manager():
+        return stx.CookieManager()
+
+    cookie_manager = get_manager()
+
+    st.subheader("All Cookies:")
+    cookies = cookie_manager.get_all()
+    st.write(cookies)
+
+    c1, c2, c3 = st.columns(3)
+
+    with c1:
+        st.subheader("Get Cookie:")
+        cookie = st.text_input("Cookie", key="0")
+        clicked = st.button("Get")
+        if clicked:
+            value = cookie_manager.get(cookie=cookie)
+            st.write(value)
+    with c2:
+        st.subheader("Set Cookie:")
+        cookie = st.text_input("Cookie", key="1")
+        val = st.text_input("Value")
+        if st.button("Add"):
+            cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+    with c3:
+        st.subheader("Delete Cookie:")
+        cookie = st.text_input("Cookie", key="2")
+        if st.button("Delete"):
+            cookie_manager.delete(cookie)
+    
+    
 #total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 #num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 #test=st.slider("Test", "08.03.2021", "11.01.2022")
