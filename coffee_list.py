775bbde0724af2e4ef98ce66464848248cf36092
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-18 12:25:03 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -344,7 +344,7 @@ if logged_in == "true" and profile_nav == "Show diagrams":
         df["e"] = stdev
 
         info = func_select
-        fig8 = px.scatter(df, x=names, y='Number of coffees', error_y='e', title="Exp. values ±σ for "+months[len(months)-1], labels={"x":"", "y":"Number of coffees", "variable":"drinkers"}, text="Number of coffees")
+        fig8 = px.line(df, x=names, y='Number of coffees', error_y='e', title="Exp. values ±σ for "+months[len(months)-1], labels={"x":"", "y":"Number of coffees", "variable":"drinkers"}, text="Number of coffees")
         fig8.update_layout(title_font_size=24, showlegend=False)
         fig8.update_traces(hovertemplate='%{x}: %{y}', marker = dict(symbol = 'line-ew-open'), textposition='middle right')
         fig8.update_yaxes(range=[0,max(max_values)+2])
