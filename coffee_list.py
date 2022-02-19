e903e51b974697f9f1d02455eb36061dbf0663f7
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-19 15:35:25 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -54,12 +54,12 @@ last_breaks=get_last_breaks()
 all_func=get_functionals()
 act_func=get_active_func()
 
-if 'logged_in' not in st.session_state:
-    if cookie_manager.get(cookie="logged_in") == "true":
-        st.write("passed")
-        st.session_state.logged_in="true"
-    else:
-        st.session_state.logged_in="false"
+#if 'logged_in' not in st.session_state:
+if cookie_manager.get(cookie="logged_in") == "true":
+    st.write("passed")
+    st.session_state.logged_in="true"
+    #else:
+        #st.session_state.logged_in="false"
 if 'user_name' not in st.session_state:
     st.session_state.user_name=cookie_manager.get(cookie="user")
 if 'admin' not in st.session_state:
