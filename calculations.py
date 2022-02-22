140042b8fb8eb73885ed59a0934fe78f1b585768
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-22 23:52:04 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -60,15 +60,14 @@ def write_simple_data():
 		if coffees[0][len(month_id)-3][i] != 0 and coffees[0][len(month_id)-2][i] != 0:
 			act_dr += 1
 	cursor.execute("update simple_data set value = "+str(len(names))+" where parameter = 'drinkers'")	#updating simple_data table
-	st.write("1")
+	db.commit()
 	cursor.execute("update simple_data set value = "+str(act_dr)+" where parameter = 'acr_dr'")
-	st.write("2")
+	db.commit()
 	cursor.execute("update simple_data set value = "+str(len(month_id))+" where parameter = 'months'")
-	st.write("3")
+	db.commit()
 	cursor.execute("update simple_data set value = "+str(breaks[0][0])+" where parameter = 'breaks'")
-	st.write("4")
+	db.commit()
 	cursor.execute("update simple_data set value = "+str(cups)+" where parameter = 'cups'")
-	st.write("5")
 	db.commit()
 	return simple_data
 
