4f25298e7ffbe121924a3b3bfa8d2169a1e8af4f
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-22 19:16:29 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -332,7 +332,7 @@ if logged_in == "true" and profile_nav == "Show diagrams":
     month_info=get_months(datetime.date(2020,11,1))
     months_all=month_info[0]
     month_id_all=month_info[1]
-    monthly_coffees = get_monthly_coffees(names, month_id_all)
+    
     
     
     
@@ -376,6 +376,7 @@ if logged_in == "true" and profile_nav == "Show diagrams":
     if coffees_monthly:
         st.subheader("Coffees per month") 
         
+        monthly_coffees = get_monthly_coffees()#names, month_id_all)
         df = pd.DataFrame(monthly_coffees, columns=names, index=months_all)    #coffees per month per person
 
         fig1 = px.line(df, title="Number of coffees per month per person", labels={"variable":"", "index":"", "value":"Number of coffees"})
