84ed8882ac10307c068fe45755f42be8fe5a4457
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-22 19:44:11 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -257,7 +257,7 @@ if logged_in == "true":
         st.markdown("Please enter the extended ID of the break you want to delete.")
         last_break=1
         last_breaks=get_last_breaks(last_break)
-        max_id = last_breaks[len(last_breaks())-1][0]
+        max_id = str(last_breaks[len(last_breaks())-1][0])
         
         col1,col2,col3 = st.columns([1,0.5,3])
         col1.text_input("Extended ID of break", placeholder=str(max_id))
