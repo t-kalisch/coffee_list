2674291a27626fa0b8bd2c379b4493d41424b8e6
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-23 12:12:37 +0100

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -18,7 +18,7 @@ cursor=db.cursor(buffered=True)
 def get_user_data():
 	cursor.execute("select name, password, admin from members")
 	user_data=cursor.fetchall()
-	print(user_data)
+	st.write(user_data)
 	return user_data
 
 
