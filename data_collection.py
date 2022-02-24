f249f1c6055e8c4e2f40e562ba5353690be70b39
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-24 10:08:28 +0100

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -4,11 +4,6 @@ import mysql.connector as mysql
 #import csv
 from calculations import *
 
-@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
-
-
 conn = init_connection()
 cursor = conn.cursor()
 
