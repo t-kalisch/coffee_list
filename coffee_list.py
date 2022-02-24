d41f3ca5c38314de2fa1d5375905f7ed93ae24d0
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-02-24 14:37:39 +0100

    Update coffee_list.py

--- coffee_list.py
+++ coffee_list.py
@@ -328,7 +328,7 @@ with st.sidebar:
     st.title("Available diagrams:")
     coffees_monthly = st.checkbox("Monthly coffees")
     coffees_total = st.checkbox("Total coffees / Monthly ratios")
-    expectation_data = st.checkbox("Expectation values / Coffee prize history")
+    expectation_data = st.checkbox("Expectation values / Prize history")
     c_b_weekly = st.checkbox ("Weekly breaks and coffees")
     correlation = st.checkbox("Correlation")
     break_percentage = st.checkbox("Percentages of breaks")
@@ -449,7 +449,7 @@ if logged_in == "true" and profile_nav == "Show diagrams":
         df["e"] = stdev
 
         info = act_func
-        fig8 = px.scatter(df, x=names, y='Number of coffees', error_y='e', title="Exp. values  ± σ for "+months_all[len(months_all)-1], labels={"x":"", "y":"Number of coffees", "variable":"drinkers"}, text="Number of coffees")
+        fig8 = px.scatter(df, x=names, y='Number of coffees', error_y='e', title="Expect. val.  ± σ for "+months_all[len(months_all)-1], labels={"x":"", "y":"Number of coffees", "variable":"drinkers"}, text="Number of coffees")
         fig8.update_layout(title_font_size=24, showlegend=False)
         fig8.update_traces(marker = dict(symbol = 'line-ew-open'), hovertemplate='%{x}: %{y}', textposition='middle right')
         fig8.update_yaxes(range=[0,max(max_values)+2])
@@ -480,10 +480,10 @@ if logged_in == "true" and profile_nav == "Show diagrams":
                     total += 1
             total_prizes.append(total)
 
-        columns=['Month','Persons','','sizes']
+        columns=['Month','Persons','Coffee prizes','sizes']
         df = pd.DataFrame(prizes, columns=columns)
 
-        fig2 = px.scatter(df, x='Month', y='Persons', title="Coffee prize history ("+act_func+")", labels={"variable":"", "index":"", "value":""}, size='sizes', color='prize', color_discrete_sequence=['gold','black','red'])      #plotting social score
+        fig2 = px.scatter(df, x='Month', y='Persons', title="Coffee prize history ("+act_func+")", labels={"variable":"", "index":"", "value":""}, size='sizes', color='Coffee prizes', color_discrete_sequence=['gold','black','red'])      #plotting social score
         fig2.update_layout(title_font_size=24, yaxis=dict(tickmode = 'array', tickvals = tickval_num, ticktext = names), hovermode="x unified", xaxis=dict(tickmode = 'array', tickvals = month_id_dly, ticktext = months_dly), legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5))
         fig2.update_traces(hovertemplate='%{y}')
         col1.plotly_chart(fig2, use_container_width=True)
