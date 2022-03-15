df72ebca483b9a38e0a5d61562460fdf516973a0
t-kalisch <97513797+t-kalisch@users.noreply.github.com>
2022-03-15 12:49:16 +0100

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -226,7 +226,6 @@ def add_coffee_to_break(id_ext, name, user):
 		name = user
 	cursor.execute("select persons, coffees from drinkers where id_ext = '"+id_ext+"'")
 	drinker_data=list(cursor.fetchall()[0])
-	st.write(drinker_data)
 	if drinker_data == []:
 		st.warning("Invalid extended ID")
 		return
